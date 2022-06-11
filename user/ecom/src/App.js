549aa134f29f1cbdde299426ca2b5aa138452e21
noor <NoorMohammed3399@gmail.com>
2022-06-11 10:59:15 +0200

    configure1

---
+++ user/ecom/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button variant="warning">warning</Button>
+        <Button variant="info"><i className="fa fa-home"></i></Button>
+
+      </div>
+    )
+  }
+}
+
+export default App;
