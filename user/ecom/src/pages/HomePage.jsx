4bd2b913fbd854270b0f8c0eaebd15bf7f09c341
noor <NoorMohammed3399@gmail.com>
2022-06-11 23:38:39 +0200

    Categories Component

--- user/ecom/src/pages/HomePage.jsx
+++ user/ecom/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from 'react'
+import Categories from '../components/home/Categories'
 import FeaturedProducts from '../components/home/FeaturedProducts'
 export class HomePage extends Component {
     render() {
         return (
             <Fragment>
                 <FeaturedProducts />
+                <Categories/>
             </Fragment>
         )
     }
