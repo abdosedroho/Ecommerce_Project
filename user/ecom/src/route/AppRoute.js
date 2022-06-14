f6fee030a7283f4defc2c868aebfe130ca337988
MayarANageeb <mayar.18332504@compit.aun.edu.eg>
2022-06-14 15:30:46 +0200

    User Login Register Component

--- user/ecom/src/route/AppRoute.js
+++ user/ecom/src/route/AppRoute.js
@@ -1,15 +1,16 @@
 import React, { Component, Fragment } from 'react'
 import { Router, Route, Switch } from "react-router-dom";
 import HomePage from '../pages/HomePage';
+import UserLoginPage from '../pages/UserLoginPage';
 
 class AppRoute extends Component {
     render() {
         return (
             <Fragment>
-                <Switch>
-                    <Route exact path="/" component={HomePage} />
-                </Switch>
-
+            <Switch>
+                 <Route exact path="/" component={HomePage} />
+                 <Route exact path="/login" component={UserLoginPage} />
+            </Switch>
             </Fragment>
         )
     }
