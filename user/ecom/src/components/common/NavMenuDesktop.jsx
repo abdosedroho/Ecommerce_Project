c42c8b4871e1d94ec19e5d6559a09d4699753ea3
MayarANageeb <mayar.18332504@compit.aun.edu.eg>
2022-06-14 18:36:41 +0200

    Update Nave Menu Icon

--- user/ecom/src/components/common/NavMenuDesktop.jsx
+++ user/ecom/src/components/common/NavMenuDesktop.jsx
@@ -3,6 +3,7 @@ import {Navbar,Container, Row, Col,Button} from 'react-bootstrap';
 import Logo from '../../assets/images/easyshop.png'
 import {Link} from "react-router-dom";
 import MegaMenuAll from '../home/MegaMenuAll';
+import Bars from '../../assets/images/bars.png';
 class NavMenuDesktop extends Component {
     constructor(){
         super();
@@ -39,7 +40,7 @@ SideNavOpenClose=()=>{
     <Container fluid={"true"} className="fixed-top shadow-sm p-2 mb-0 bg-white">
       <Row>
           <Col lg={4} md={4} sm={12} xs={12}>
-          <Button onClick={this.MenuBarClickHandler} className="btn"><i className="fa fa-bars"></i>  </Button>
+          <img onClick={this.MenuBarClickHandler} className="bar-img" src={Bars} />
           <Link to="/" ><img className="nav-logo" src ={Logo}/></Link>
           </Col>
           <Col className="p-1 mt-1"  lg={4} md={4} sm={12} xs={12}>
