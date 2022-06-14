6aa0ccb4339de08a5d418474b7424008dab445ea
fatmaabdelhakem <fatmaabdelhakem>
2022-06-14 13:04:29 +0200

    Design footer and other component

--- user/ecom/src/pages/HomePage.jsx
+++ user/ecom/src/pages/HomePage.jsx
@@ -1,8 +1,12 @@
 import React, { Component, Fragment } from 'react'
+import { FooterDesktop } from '../components/common/FooterDesktop'
+import NavMenuDesktop from '../components/common/NavMenuDesktop'
+import NavMenuMobile from '../components/common/NavMenuMobile'
 import Categories from '../components/home/Categories'
 import Collection from '../components/home/Collection'
 import FeaturedProducts from '../components/home/FeaturedProducts'
 import HomeTop from '../components/home/HomeTop'
+import HomeTopMobile from '../components/home/HomeTopMobile'
 import NewArrival from '../components/home/NewArrival'
 export class HomePage extends Component {
     render() {
@@ -10,11 +14,23 @@ export class HomePage extends Component {
         return (
             
             <Fragment>
-                <HomeTop/>
+                <div className="Desktop">
+               <NavMenuDesktop />
+               <HomeTop />
+                    </div>
+
+                    <div className="Mobile">
+                    <NavMenuMobile />  
+                    <HomeTopMobile />
+                    </div>                       
+
+               
+        
                 <FeaturedProducts />
                 <NewArrival/>
-                <Collection/>
                 <Categories/>
+                <Collection/>
+                <FooterDesktop/>
             </Fragment>
         )
     }
