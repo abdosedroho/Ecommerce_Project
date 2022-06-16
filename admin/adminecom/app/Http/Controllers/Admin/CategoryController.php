8bd04ce676d0f75dfcb19b724d0bcd58edcdeb3f
MayarANageeb <mayar.18332504@compit.aun.edu.eg>
2022-06-16 04:58:25 +0200

    Select Category Subcategory And Make JSON Array

--- admin/adminecom/app/Http/Controllers/Admin/CategoryController.php
+++ admin/adminecom/app/Http/Controllers/Admin/CategoryController.php
@@ -5,11 +5,24 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Subcategory;
 
 class CategoryController extends Controller
 {
     public function AllCategory(){
+
         $categories =Category::All();
-        return  $categories;
-     }// End Method
+        $categoryDetailsArray =[];
+        foreach ($categories as $value)
+        {
+            $Subcategory= Subcategory::where('category_name',$value['category_name'])->get();
+            $item =[
+                'category_name'=>$value['category_name'],
+                'category_iamge'=>$value['category_name'],
+                'subcategory_name'=>$Subcategory
+            ];
+            array_push($categoryDetailsArray,$item);
+        }
+        return $categoryDetailsArray;
+    }
 }
