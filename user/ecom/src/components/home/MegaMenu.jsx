41e3467f6af75c19eb503a748ef2d1a493e8d996
fatmaabdelhakem <fatmaabdelhakem>
2022-06-22 00:54:16 +0200

    Create Product List API

--- user/ecom/src/components/home/MegaMenu.jsx
+++ user/ecom/src/components/home/MegaMenu.jsx
@@ -1,52 +1,69 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from 'react'
+import { Link } from 'react-router-dom';
 
 class MegaMenu extends Component {
-    constructor(props){
-        super();
-        
-    }
-    MenuItemClick=(event)=>{
-        event.target.classList.toggle("active");
-        var panel = event.target.nextElementSibling;
-        if(panel.style.maxHeight){
-             panel.style.maxHeight = null;
-        }else{
-             panel.style.maxHeight= panel.scrollHeight+ "px"
-        }
-    }
 
-    render() {
+     constructor(props){
+          super();
+           
+     }
+ 
+ 
 
-        const CatList = this.props.data;
-        const MyView = CatList.map((CatList,i)=>{
-            return <div key={i.toString()}>
-    <button onClick={this.MenuItemClick} className="accordion">
-   <img className="accordionMenuIcon" src={CatList.category_image} />&nbsp; {CatList.category_name}
-                    </button>
-                    <div className="panel">
+     MenuItemClick=(event)=>{
+          event.target.classList.toggle("active");
+          var panel = event.target.nextElementSibling;
+          if(panel.style.maxHeight){
+               panel.style.maxHeight = null;
+          }else{
+               panel.style.maxHeight= panel.scrollHeight+ "px"
+          }
+
+     }
+
+
+     render() { 
+
+          const CatList = this.props.data;
+
+          const MyView = CatList.map((CatList,i)=>{
+               return <div key={i.toString()}>
+      <button onClick={this.MenuItemClick} className="accordion">
+      <img className="accordionMenuIcon" src={CatList.category_image} />&nbsp; {CatList.category_name}
+                        </button>
+          <div className="panel">
       <ul>
-        {
-               (CatList.Subcategory_name).map((SubList,i)=>{
-                    return <li><a href="#" className="accordionItem" >{SubList.Subcategory_name} </a></li>
+          {
+               (CatList.subcategory_name).map((SubList,i)=>{
+                    return <li><Link to={"productsubcategory/"+CatList.category_name+"/"+SubList.subcategory_name } className="accordionItem" >{SubList.subcategory_name} </Link></li>
 
                })    
           }
-            </ul>
+          
+      </ul>
          </div> 
-
+             
                </div>
-                });
-                return (
-                    <div className="accordionMenuDiv">
-                         <div className="accordionMenuDivInside">
-      
-                     {MyView}
 
-                </div>
 
-            </div>
-        )
-    }
+
+          });
+
+
+
+
+
+          return (
+              <div className="accordionMenuDiv">
+                   <div className="accordionMenuDivInside">
+
+               {MyView}
+   
+                   </div>
+
+              </div>
+          )
+     }
 }
 
-export default MegaMenu;
+export default MegaMenu
